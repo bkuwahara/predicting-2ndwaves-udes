9eada90c589c4f2d7890c92f541dabd6dacf853a
Bruce Kuwahara <bmkuwaha@uwaterloo.ca>
2022-08-16 23:56:23 -0300

    Added some lines to udde_ensemble to account for predictions that failed due to instability

--- scripts/udde_ensemble.jl
+++ scripts/udde_ensemble.jl
@@ -32,16 +32,21 @@ function EnsembleSummary(sim_name::String, region::String, hdims::Int, τₘ::Ab
 
 	f = load(joinpath(root, filenames[1])* "/results.jld2")
 	pred = f["prediction"]
-	for fname in filenames[2:end]
-		f = load(joinpath(root, fname)* "/results.jld2")
-		pred = hvncat(3, pred, f["prediction"])
+	for fn in filenames[2:end]
+		f = load(joinpath(root, fn)* "/results.jld2")
+		new_pred = f["prediction"]
+		if size(new_pred, 2) != size(pred, 2)
+			new_pred =  hcat(new_pred, NaN .*zeros(size(new_pred,1), size(pred, 2)-size(new_pred, 2)))
+		end
+		pred = hvncat(3, pred, new_pred)
 	end
 
 	mean_pred = mean(pred, dims=3)
 	sd_pred = std(pred, dims=3)
-    med_pred = median(pred, dims=3)
-    qu = [quantile(pred[i,j,:], 0.75) for i in axes(pred,1), j in axes(pred,2)]
-    ql = [quantile(pred[i,j,:], 0.25) for i in axes(pred,1), j in axes(pred,2)]
+	med_pred = median(pred, dims=3)
+	qu = [isnan(mean_pred[i,j,1]) ? NaN : quantile(pred[i,j,:], 0.75) for i in axes(pred,1), j in axes(pred,2)]
+	ql = [isnan(mean_pred[i,j,1]) ? NaN : quantile(pred[i,j,:], 0.25) for i in axes(pred,1), j in axes(pred,2)]
+	
 	return mean_pred[:,:,1], med_pred[:,:,1], sd_pred[:,:,1], qu, ql
 end
 
@@ -61,12 +66,7 @@ function EnsemblePlot(sim_name::String, region::String, hdims::Int, τₘ::Abstr
 		ylabel=hcat(["S" "I"], reshape([indicator_names[i] for i in indicators], 1, length(indicators))))
 
 	pred_tsteps = range(max(τᵣ+1, τₘ), length=size(μ,2), stop=all_tsteps[end])
-	# q = quantile(Normal(), (CI+1)/2)
-	# ribbon_lower = zeros(size(μ))
-	# ribbon_lower[1,:] .= q*sd[1,:]
-	# ribbon_lower[3,:] .= q*sd[3,:]
-	# ribbon_lower[2,:] .= [μ[2,i] - q*sd[2,i] < 0 ? μ[2,i] : q*sd[2,i] for i in eachindex(μ[2,:])]
-	# ribbon_upper = q*sd'
+
 	plot!(pl, pred_tsteps, med', color=:red, ribbon = ((med-ql)', (qu-med)'), label=["Median prediction (IQR)" nothing nothing nothing nothing nothing])
     ylims!(pl[3], (minimum(data[3,:]) - 2, maximum(data[3,:]) + 2))
 	train_stop = Int(round(0.75*90))
@@ -100,7 +100,7 @@ for country_region in ["AT" "NL" "UK" "CA-ON" "US-NY" ]
 end
 
 
-EnsemblePlot("ensemble_NY", "US-NY", 3, 10.0, 14.0, (1, 5, 5))
+EnsemblePlot("baseline_NY", "US-NY", 3, 10.0, 14.0, (1, 0, 0))
 
 
 
