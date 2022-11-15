be7adf85e64b2163b8f76b7cc4da7ff666b35b69
Bruce Kuwahara <bmkuwaha@uwaterloo.ca>
2022-11-14 23:28:52 -0400

    Adding in final analysis/plotting functions

--- scripts/udde_ensemble.jl
+++ scripts/udde_ensemble.jl
@@ -3,7 +3,7 @@ using DrWatson
 @quickactivate("S2022_Project")
 
 using LinearAlgebra
-using JLD2, FileIO, CSV
+using JLD2, FileIO, CSV, DataFrames
 using Dates
 using Plots
 using Statistics
@@ -32,10 +32,8 @@ loss_labels = [
 	"accuracy"; 
 	"beta_mon"; 
 	"beta_pos"; 
-	"f_lb";
 	"f_ub";
 	"f_basetend";
-	"basetend_mon_M";
 	"basetend_mon_R";
 	"f_mon_I";
 	"f_mon_deltaI";
@@ -64,36 +62,34 @@ function l_layer1_avg(p, Ms)
 	beta_j = network1(Ms .+ ϵ, p, st1)[1]
 
 	l1 = sum(relu.( -beta_i))
-	l5 = sum(relu.( beta_i .- beta_j))
+	l2 = sum(relu.( beta_i .- beta_j))
 
-	return [l1, l5]./length(Ms)
+	return [l1, l2]./size(Ms,2)
 end
 
 
-
-function l_layer2(p, Is, ΔIs, Ms, Rs)
-	n_pts = size(Is,2)
-	M_min = mobility_min .* ones(1, n_pts)
-	I_baseline = zeros(1, n_pts)
-	M_max = 2*(mobility_baseline - mobility_min) .* ones(1,n_pts)
+M_min = mobility_min .* ones(1, n_pts)
+I_baseline = zeros(1, n_pts)
+M_max = 2*(mobility_baseline - mobility_min) .* ones(1,n_pts)
+function l_layer2_avg(p, Is, ΔIs, Ms, Rs)
 	# Must not decrease when M at M_min
 	dM_min = network2([M_min; Is; ΔIs; Rs], p, st2)[1]
-	l6 = sum(relu.(-dM_min))
+	l3 = sum(relu.(-dM_min))
 
 	# Must not increase when M at 2*(mobility_baseline - mobility_min)
 	dM_max = network2([M_max; Is; ΔIs; Rs], p, st2)[1]
-	l6 = sum(relu.(dM_max))
+	l4 = sum(relu.(dM_max))
 
 	# Tending towards M=mobility_baseline when I == ΔI == 0
 	dM1_baseline = network2([Ms; I_baseline; I_baseline; Rs], p, st2)[1]
 	dM2_baseline = network2([Ms .+ ϵ; I_baseline; I_baseline; Rs], p, st2)[1]
 	dM3_baseline = network2([Ms; I_baseline; I_baseline; Rs .+ ϵ], p, st2)[1]
-	l2 = sum(relu, relu.(dM1_baseline .* (Ms .- mobility_baseline)))
+	l5 = sum(relu, relu.(dM1_baseline .* (Ms .- mobility_baseline)))
 
 	# Stabilizing effect is stronger at more extreme M and higher R
 	sgn_M = abs.(Ms) .- abs.(Ms .+ ϵ)
 	sgn_dM = abs.(dM1_baseline) .- abs.(dM2_baseline)
-	l3 = sum(relu.(-1 .* sgn_M .* sgn_dM))
+	l6 = sum(relu.(-1 .* sgn_M .* sgn_dM))
 	l7 = sum(relu.(abs.(dM1_baseline) .- abs.(dM3_baseline)))
 
 	## Monotonicity terms
@@ -101,85 +97,80 @@ function l_layer2(p, Is, ΔIs, Ms, Rs)
 
 	# f monotonically decreasing in I
 	dM_deltaI = network2([Ms; (Is .+ ϵ); ΔIs; Rs], p, st2)[1]
-	l3 = sum(relu.(dM_deltaI .- dM_initial))
+	l8 = sum(relu.(dM_deltaI .- dM_initial))
 	
 	# f monotonically decreasing in ΔI
 	dM2_delta_deltaI = network2([Ms; Is; ΔIs .+ ϵ; Rs], p, st2)[1]
-	l4 = sum(relu.(dM2_delta_deltaI .- dM_initial))
-	return [l2, l3, l4, l6, l7]./length(Ms)
+	l9 = sum(relu.(dM2_delta_deltaI .- dM_initial))
+	return [l3, l4, l5, l6, l7, l8, l9]./size(Ms,2)
 end
 
 
-
-
-
-function EnsembleSummary(sim_name::String, region::String, hdims::Int, τₘ::AbstractFloat, τᵣ::AbstractFloat)
+function EnsembleSummary(sim_name)
 	root = datadir("sims", "udde", sim_name)
-	indicator_name = "rr"
-	params = (taum = τₘ, taur = τᵣ, hdims=hdims)
-	param_name = savename(params)
 
-	fname = "$(region)_$(indicator_name)_$(param_name)"
-	filenames = filter(s->rsplit(s, "_", limit=2)[1] == fname, readdir(root))
+	filenames = filter(f -> isdir(root*"\\"*f), readdir(root))
 	f = load(joinpath(root, filenames[1])* "/results.jld2")
 	pred = f["prediction"]
+	betas = f["betas"]
 	for fn in filenames[2:end]
 		f = load(joinpath(root, fn)* "/results.jld2")
 		new_pred = f["prediction"]
+		new_betas = f["betas"]
 		if size(new_pred, 2) != size(pred, 2)
 			new_pred =  hcat(new_pred, NaN .*zeros(size(new_pred,1), size(pred, 2)-size(new_pred, 2)))
 		end
 		pred = hvncat(3, pred, new_pred)
+		betas = hvncat(3, betas, new_betas)
 	end
 
 	mean_pred = mean(pred, dims=3)
-	sd_pred = std(pred, dims=3)
-	med_pred = median(pred, dims=3)
-	qu = [isnan(mean_pred[i,j,1]) ? NaN : quantile(pred[i,j,:], 0.75) for i in axes(pred,1), j in axes(pred,2)]
-	ql = [isnan(mean_pred[i,j,1]) ? NaN : quantile(pred[i,j,:], 0.25) for i in axes(pred,1), j in axes(pred,2)]
-	
-	return mean_pred[:,:,1], med_pred[:,:,1], sd_pred[:,:,1], qu, ql
-end
+	med_pred = median(pred, dims=3)[:,:,1]
+	qu_pred = [isnan(mean_pred[i,j,1]) ? NaN : quantile(pred[i,j,:], 0.75) for i in axes(pred,1), j in axes(pred,2)]
+	ql_pred = [isnan(mean_pred[i,j,1]) ? NaN : quantile(pred[i,j,:], 0.25) for i in axes(pred,1), j in axes(pred,2)]
+
+	med_betas = median(betas, dims=3)[:,:,1]
+	qu_betas = [quantile(betas[i,j,:], 0.75) for i in axes(betas,1), j in axes(betas,2)]
+	ql_betas = [quantile(betas[i,j,:], 0.25) for i in axes(betas,1), j in axes(betas,2)]
 
 
-
-function EnsemblePlot(sim_name::String, region::String, hdims::Int, τₘ::AbstractFloat, τᵣ::AbstractFloat;
-	showplot=true, saveplot=true)
-	
-	μ, med, sd, qu, ql = EnsembleSummary(sim_name, region, hdims, τₘ, τᵣ)
-
-	indicators = [3]
-	dataset = load(datadir("exp_pro", "SIMX_7dayavg_roll=false_$(region).jld2"))
+	region = split(sim_name, "_")[end]
+	indicators=[3]
+	dataset = load(datadir("exp_pro", "SIMX_final_7dayavg_roll=false_$(region).jld2"))
 	data = dataset["data"][hcat([1 2], indicators), :][1,:,:]
 	days = dataset["days"]
 	all_tsteps = range(0, step=7, length=size(data,2))
-	pl = scatter(all_tsteps, data', layout=(size(data,1), 1), color=:black, label=["Data" nothing nothing nothing nothing nothing],
+	pl_pred = scatter(all_tsteps, data', layout=(size(data,1), 1), color=:black, label=["Data" nothing nothing nothing nothing nothing],
 		ylabel=hcat(["S" "I"], reshape([indicator_names[i] for i in indicators], 1, length(indicators))))
 
-	pred_tsteps = range(max(τᵣ+1, τₘ), length=size(μ,2), stop=all_tsteps[end])
 
-	plot!(pl, pred_tsteps, med', color=:red, ribbon = ((med-ql)', (qu-med)'), label=["Median prediction (IQR)" nothing nothing nothing nothing nothing])
-    ylims!(pl[3], (minimum(data[3,:]) - 2, maximum(data[3,:]) + 2))
-	train_stop = Int(round(0.75*90))
-	for i in 1:size(data, 1)-1
-		vline!(pl[i], [15+train_stop], color=:black, style=:dash, label=nothing)
-	end
-	vline!(pl[end], [15+train_stop], color=:black, style=:dash, label="End of training data")
 
+	pred_tsteps = range(14.0, step=1.0, length=size(med_pred,2))
+	plot!(pl_pred, pred_tsteps, med_pred', color=:red, ribbon = ((med_pred-ql_pred)', (qu_pred-med_pred)'), label=["Median prediction (IQR)" nothing nothing])
+	ylims!(pl[3], (minimum(data[3,:]) - 2, maximum(data[3,:]) + 2))
 	xlabel!(pl[end], "Time (days since $(days[1]))")
-	title!(pl[1], "Ensemble prediction, LSTM model, $(region)")
+	title!(pl[1], "Ensemble prediction, $(region)")
 
+
+
+	M_test = range(mobility_min, step=0.1, stop=2*(mobility_baseline - mobility_min))
+	pl_betas = plot(M_test, med_betas', ribbon=((med_betas-ql_betas)', (qu_betas-med_betas)'), label=nothing)
+	xlabel!(pl_betas, "M")
+	ylabel!(pl_betas, "β")
+	vline!(pl_betas, [minimum(data[3,:]) maximum(data[3,:])], color=:black, label=["Observed range" nothing], 
+				style=:dash)
 
 	if showplot
-		display(pl)
+		display(pl_pred)
+		display(pl_betas)
 	end
 	if saveplot
-		indicator_name = "rr"
-		param_name = "hdims=$(hdims)_taum=$(τₘ)_taur=$(τᵣ)"
-		fname = "ensemble_$(region)_$(indicator_name)_$(param_name)"
-		savefig(pl, datadir("sims", "udde", sim_name, fname*".png"))
+		savefig(pl_pred, datadir("sims", "udde", sim_name, "ensemble_pred.png"))
+		savefig(pl_betas, datadir("sims", "udde", sim_name, "ensemble_beta_response.png"))
+
 	end
 	return nothing	
+
 end
 
 
@@ -257,36 +248,55 @@ end
 
 
 
-# function loss_analysis(sim_name)
-root = datadir("sims", "udde", sim_name)
-filenames = readdir(root)
+function loss_analysis(sim_name)
+	root = datadir("sims", "udde", sim_name)
+	filenames = readdir(root)
 
-all_losses = zeros(8, length(filenames))
+	all_losses = zeros(10, length(filenames))
+	loss_weight = 0
 
-	# for fname in filenames
-results = load(datadir("sims", "udde", sim_name, fname, "results.jld2"))
-scale = results["scale"]
-losses = results["losses"]
-pred = results["prediction"]
-hist_data = results["hist_data"]
-train_data = results["train_data"]
-test_data = results["test_data"]
-β = results["betas"]
-p = results["p"]
+	for (i, fname) in enumerate(filenames)
+		
+		results = load(datadir("sims", "udde", sim_name, fname, "results.jld2"))
+		scale = results["scale"]
+		losses = results["losses"]
+		pred = results["prediction"]
+		hist_data = results["hist_data"]
+		train_data = results["train_data"]
+		test_data = results["test_data"]
+		β = results["betas"]
+		p = results["p"]
 
-acc_loss = minimum(losses[1,:])
+		if i == 1
+			loss_weight = results["loss_weights"]
+		end
 
-Is, ΔIs, Ms, Rs = get_inputs(1000000, size(train_data, 2)*scale)
-mean_layer1_loss = l_layer1_avg(p.layer1, Ms)
-mean_layer2_loss = l_layer2(p.layer2, Is, ΔIs, Ms, Rs)
+		acc_loss = minimum(losses[1,:])
 
-mean_loss = [acc_loss; mean_layer1_loss; mean_layer2_loss]
-all_losses[:,1] = mean_loss
+		Is, ΔIs, Ms, Rs = get_inputs(1000000, size(train_data, 2)*scale)
+		mean_layer1_loss = l_layer1_avg(p.layer1, Ms)
+		mean_layer2_loss = l_layer2_avg(p.layer2, Is, ΔIs, Ms, Rs)
+
+		mean_loss = [acc_loss; mean_layer1_loss; mean_layer2_loss]
+		all_losses[:,i] = mean_loss
+
+	end
+
+	med_losses = reshape(median(all_losses, dims=2), size(all_losses,1))
+	summary = Dict{String, Any}(zip(["med_" * label for label in loss_labels], med_losses))
 
 
+	net_losses = all_losses[1,:] .+ transpose(loss_weight .* sum(all_losses[2:end,:], dims=1))
+	best_loss, best_ind = findmin(net_losses)
+	best_ver = parse(Int64, rsplit(filenames[best_ind[1]], "v")[end])
+	summary["best_ver"] = best_ver
 
-# end
+	df = DataFrame(summary)
+	CSV.write(root*"\\loss_summary.csv", df)
 
+	converged_idxs = reshape(reduce(*, all_losses .<= 0.05, dims=1), length(filenames))
+	converged_sims = filenames[converged_idxs]
+	save(root * "\\converged_sims.jld2", "sims", converged_sims)
 
-nothing
+	nothing
 end
